b334363e30f019a3c59cbd06ff66171ca5132c7d
John Rey <adioskid@mail.ru>
2024-08-24 14:21:03 +0400

    commit

--- src/main/java/com/app/springmoon/controllers/MyController.java
+++ src/main/java/com/app/springmoon/controllers/MyController.java
@@ -1,4 +1,12 @@
 package com.app.springmoon.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MyController {
+    @GetMapping
+    public String meetText() {
+        return "test";
+    }
 }
